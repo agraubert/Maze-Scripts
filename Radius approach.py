1c43a7e6d8b72fc1aa7f0caf82a9537d1485df50
captianjroot <captianjroot@live.com>
2013-07-23 23:06:56 -0500

    reorganized system to prioritize checking directions closer to the goal

--- Radius approach.py
+++ Radius approach.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from math import copysign
 def toarray(string):
     return [a for a in string]
 
@@ -13,22 +14,28 @@ def seeker(file, character):
     return y,x
 counter=0
 
-def generateset(age, locations, frontfile):
+def cost(prv_age, cur_y, cur_x):
+    global endcoordinates
+    #+abs(cur_y-endcoordinates[0])+abs(cur_x-endcoordinates[1]))
+    return (prv_age+abs(cur_y-endcoordinates[0])+abs(cur_x-endcoordinates[1])))
+    
+def generateset(current, frontfile):
     newlist=[]
     global counter
+    age=current[3]
     counter+=1
-    if(frontfile[locations[0][0]-1][locations[0][1]]==" " or frontfile[locations[0][0]-1][locations[0][1]]=="X"): #up
-        frontfile[locations[0][0]-1][locations[0][1]]="V"
-        newlist.append([locations[0][0]-1, locations[0][1]])
-    if(frontfile[locations[0][0]][locations[0][1]+1]==" " or frontfile[locations[0][0]][locations[0][1]+1]=="X"): #right
-        frontfile[locations[0][0]][locations[0][1]+1]="<"
-        newlist.append([locations[0][0], locations[0][1]+1])
-    if(frontfile[locations[0][0]+1][locations[0][1]]==" " or frontfile[locations[0][0]+1][locations[0][1]]=="X"): #down
-        frontfile[locations[0][0]+1][locations[0][1]]="^"
-        newlist.append([locations[0][0]+1, locations[0][1]])
-    if(frontfile[locations[0][0]][locations[0][1]-1]==" " or frontfile[locations[0][0]][locations[0][1]-1]=="X"): #left
-        frontfile[locations[0][0]][locations[0][1]-1]=">"
-        newlist.append([locations[0][0], locations[0][1]-1])
+    if(frontfile[current[0]-1][current[1]]==" " or frontfile[current[0]-1][current[1]]=="X"): #up
+        frontfile[current[0]-1][current[1]]="V"
+        newlist.append([current[0]-1, current[1], cost(age, current[0]-1, current[1]), age+1])
+    if(frontfile[current[0]][current[1]+1]==" " or frontfile[current[0]][current[1]+1]=="X"): #right
+        frontfile[current[0]][current[1]+1]="<"
+        newlist.append([current[0], current[1]+1, cost(age, current[0], current[1]+1), age+1])
+    if(frontfile[current[0]+1][current[1]]==" " or frontfile[current[0]+1][current[1]]=="X"): #down
+        frontfile[current[0]+1][current[1]]="^"
+        newlist.append([current[0]+1, current[1], cost(age, current[0]+1, current[1]), age+1])
+    if(frontfile[current[0]][current[1]-1]==" " or frontfile[current[0]][current[1]-1]=="X"): #left
+        frontfile[current[0]][current[1]-1]=">"
+        newlist.append([current[0], current[1]-1, cost(age, current[0], current[1]-1), age+1])
     #locations=[thing[:] for thing in newlist]
     return newlist
             
@@ -48,14 +55,15 @@ print("Checking Path")
 maximum=(len(mazebase))
 maximum*=len(mazebase[0])
 agelist=[]
-agelist.append([location[0], location[1]])
+agelist.append([location[0], location[1], 0, 0])
 while((not found) and len(agelist)>0):
     maxes=[]
-    agelist+=generateset(radius, agelist, mazebase)
-    agelist.pop(0)
+    _current=agelist.pop(0)
+    agelist+=generateset(_current, mazebase)
+    agelist.sort(key=lambda x : x[2])
     if(mazebase[endcoordinates[0]][endcoordinates[1]]!="X"):
         found=True
-    radius+=1
+        radius=_current[3]
     
 print("Solution found!")
 location=[endcoordinates[0], endcoordinates[1]]
@@ -74,6 +82,7 @@ finalmap[endcoordinates[0]][endcoordinates[1]]="O"
 finalmap[startcoordinates[0]][startcoordinates[1]]="!"
 for a in range(len(finalmap)):
     print("".join(finalmap[a]))
+radius+=2
 print("Length: ",radius)
 print(counter)
 input("")
