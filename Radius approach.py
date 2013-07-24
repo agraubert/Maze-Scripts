db4ff11474b9ddd2f5cc55e6f00f6cd32f044bf0
captianjroot <captianjroot@live.com>
2013-07-24 15:29:50 -0500

    Cleaned up and removed old lines from previous versions

--- Radius approach.py
+++ Radius approach.py
@@ -1,23 +1,21 @@
 import os
 import sys
 from math import copysign
+
 def toarray(string):
     return [a for a in string]
 
 def seeker(file, character):
-    #print("finding")
     for a in range(len(file)):
         for b in range(len(file[a])):
             if file[a][b]==character:
                 x=b
                 y=a
     return y,x
-counter=0
 
 def cost(prv_age, cur_y, cur_x):
     global endcoordinates
-    #+abs(cur_y-endcoordinates[0])+abs(cur_x-endcoordinates[1]))
-    return (prv_age+abs(cur_y-endcoordinates[0])+abs(cur_x-endcoordinates[1])))
+    return (prv_age+abs(cur_y-endcoordinates[0])+abs(cur_x-endcoordinates[1]))
     
 def generateset(current, frontfile):
     newlist=[]
@@ -36,28 +34,20 @@ def generateset(current, frontfile):
     if(frontfile[current[0]][current[1]-1]==" " or frontfile[current[0]][current[1]-1]=="X"): #left
         frontfile[current[0]][current[1]-1]=">"
         newlist.append([current[0], current[1]-1, cost(age, current[0], current[1]-1), age+1])
-    #locations=[thing[:] for thing in newlist]
     return newlist
-            
+
+counter=0            
 maze_file = open(sys.argv[1], "r")
 mazebase = maze_file.readlines()
 mazebase=[toarray(line[:-1]) for line in mazebase]
-#invisible=[thing[:] for thing in mazebase]
 finalmap=[thing[:] for thing in mazebase]
 startcoordinates = seeker(mazebase, "O")
 endcoordinates = seeker(mazebase, "X")
-location=[startcoordinates[0], startcoordinates[1]]
-radius=0
 found=False
-br=False
-#invisible[location[0]][location[1]]=str(0)
 print("Checking Path")
-maximum=(len(mazebase))
-maximum*=len(mazebase[0])
 agelist=[]
-agelist.append([location[0], location[1], 0, 0])
+agelist.append([startcoordinates[0], startcoordinates[1], 0, 0])
 while((not found) and len(agelist)>0):
-    maxes=[]
     _current=agelist.pop(0)
     agelist+=generateset(_current, mazebase)
     agelist.sort(key=lambda x : x[2])
@@ -82,7 +72,7 @@ finalmap[endcoordinates[0]][endcoordinates[1]]="O"
 finalmap[startcoordinates[0]][startcoordinates[1]]="!"
 for a in range(len(finalmap)):
     print("".join(finalmap[a]))
-radius+=2
+radius+=1
 print("Length: ",radius)
-print(counter)
+print("Grid Spaces Scanned: ", counter)
 input("")
