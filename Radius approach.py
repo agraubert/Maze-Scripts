fc4d79e0c5de020bc168ee81b8f13f2443503f67
captianjroot <captianjroot@live.com>
2013-07-24 17:08:04 -0500

    Added diagonal movement

--- Radius approach.py
+++ Radius approach.py
@@ -15,25 +15,43 @@ def seeker(file, character):
 
 def cost(prv_age, cur_y, cur_x):
     global endcoordinates
-    return (prv_age+abs(cur_y-endcoordinates[0])+abs(cur_x-endcoordinates[1]))
+    ydist=abs(cur_y-endcoordinates[0])
+    xdist=abs(cur_x-endcoordinates[1])
+    return (prv_age+ydist+xdist)
     
 def generateset(current, frontfile):
     newlist=[]
+    coordx=current[1]
+    coordy=current[0]
     global counter
+    global diagonal
     age=current[3]
     counter+=1
-    if(frontfile[current[0]-1][current[1]]==" " or frontfile[current[0]-1][current[1]]=="X"): #up
-        frontfile[current[0]-1][current[1]]="V"
-        newlist.append([current[0]-1, current[1], cost(age, current[0]-1, current[1]), age+1])
-    if(frontfile[current[0]][current[1]+1]==" " or frontfile[current[0]][current[1]+1]=="X"): #right
-        frontfile[current[0]][current[1]+1]="<"
-        newlist.append([current[0], current[1]+1, cost(age, current[0], current[1]+1), age+1])
-    if(frontfile[current[0]+1][current[1]]==" " or frontfile[current[0]+1][current[1]]=="X"): #down
-        frontfile[current[0]+1][current[1]]="^"
-        newlist.append([current[0]+1, current[1], cost(age, current[0]+1, current[1]), age+1])
-    if(frontfile[current[0]][current[1]-1]==" " or frontfile[current[0]][current[1]-1]=="X"): #left
-        frontfile[current[0]][current[1]-1]=">"
-        newlist.append([current[0], current[1]-1, cost(age, current[0], current[1]-1), age+1])
+    if(diagonal):
+        if(frontfile[coordy-1][coordx+1]==" " or frontfile[coordy-1][coordx+1]=="X"): #up/right
+            frontfile[coordy-1][coordx+1]="1" #xy plane quadrant 1
+            newlist.append([coordy-1, coordx+1, cost(age, coordy-1, coordx+1), age+1])
+        if(frontfile[coordy-1][coordx-1]==" " or frontfile[coordy-1][coordx-1]=="X"): #up/left
+            frontfile[coordy-1][coordx-1]="2" #xy plane quadrant 2
+            newlist.append([coordy-1, coordx-1, cost(age, coordy-1, coordx-1), age+1])
+        if(frontfile[coordy+1][coordx+1]==" " or frontfile[coordy+1][coordx+1]=="X"): #down/right
+            frontfile[coordy+1][coordx+1]="4" #xy plane quadrant 4
+            newlist.append([coordy+1, coordx+1, cost(age, coordy+1, coordx+1), age+1])
+        if(frontfile[coordy+1][coordx-1]==" " or frontfile[coordy+1][coordx-1]=="X"): #down/left
+            frontfile[coordy+1][coordx-1]="3" #xy plane quadrant 3
+            newlist.append([coordy+1, coordx-1, cost(age, coordy+1, coordx-1), age+1])
+    if(frontfile[coordy-1][coordx]==" " or frontfile[coordy-1][coordx]=="X"): #up
+        frontfile[coordy-1][coordx]="V"
+        newlist.append([coordy-1, coordx, cost(age, coordy-1, coordx), age+1])
+    if(frontfile[coordy][coordx+1]==" " or frontfile[coordy][coordx+1]=="X"): #right
+        frontfile[coordy][coordx+1]="<"
+        newlist.append([coordy, coordx+1, cost(age, coordy, coordx+1), age+1])
+    if(frontfile[coordy+1][coordx]==" " or frontfile[coordy+1][coordx]=="X"): #down
+        frontfile[coordy+1][coordx]="^"
+        newlist.append([coordy+1, coordx, cost(age, coordy+1, coordx), age+1])
+    if(frontfile[coordy][coordx-1]==" " or frontfile[coordy][coordx-1]=="X"): #left
+        frontfile[coordy][coordx-1]=">"
+        newlist.append([coordy, coordx-1, cost(age, coordy, coordx-1), age+1])
     return newlist
 
 counter=0            
@@ -44,6 +62,7 @@ finalmap=[thing[:] for thing in mazebase]
 startcoordinates = seeker(mazebase, "O")
 endcoordinates = seeker(mazebase, "X")
 found=False
+diagonal=True
 print("Checking Path")
 agelist=[]
 agelist.append([startcoordinates[0], startcoordinates[1], 0, 0])
@@ -54,7 +73,6 @@ while((not found) and len(agelist)>0):
     if(mazebase[endcoordinates[0]][endcoordinates[1]]!="X"):
         found=True
         radius=_current[3]
-    
 print("Solution found!")
 location=[endcoordinates[0], endcoordinates[1]]
 while(location[0]!=startcoordinates[0] or location[1]!= startcoordinates[1]):
@@ -68,11 +86,23 @@ while(location[0]!=startcoordinates[0] or location[1]!= startcoordinates[1]):
         location[1]+=1
     elif(direction=="V"):
         location[0]+=1
+    elif(direction=="1"):
+        location[0]+=1
+        location[1]-=1
+    elif(direction=="2"):
+        location[0]+=1
+        location[1]+=1
+    elif(direction=="3"):
+        location[0]-=1
+        location[1]+=1
+    elif(direction=="4"):
+        location[0]-=1
+        location[1]-=1
 finalmap[endcoordinates[0]][endcoordinates[1]]="O"
 finalmap[startcoordinates[0]][startcoordinates[1]]="!"
 for a in range(len(finalmap)):
     print("".join(finalmap[a]))
 radius+=1
 print("Length: ",radius)
-print("Grid Spaces Scanned: ", counter)
+print("Movement Sets Created: ", counter)
 input("")
